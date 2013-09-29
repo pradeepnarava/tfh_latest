2b338da2b24b1408a38145fb64f1e2efb7aa24b0
Malkit Singh <malkit@ymail.com>
2013-09-30 00:31:32 +0530

    changed ovningar button color to light blue as usual in other places

--- Livskompassen.m
+++ Livskompassen.m
@@ -31,16 +31,47 @@
     [super viewDidLoad];
     self.navigationItem.title=@"Livskompassen";
     
-    UIBarButtonItem *btnDone = [[UIBarButtonItem alloc] initWithTitle:@"Tillbaka" style:UIBarButtonItemStyleBordered target:nil action:nil];
-    // [btnDone setTintColor:[UIColor Color]];
-    UIImage *stretchable = [UIImage imageNamed:@"tillbakabutton.png"] ;
-    [btnDone setBackButtonBackgroundImage:stretchable forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-    [self.navigationController.navigationItem setBackBarButtonItem:btnDone];
+//    UIBarButtonItem *btnDone = [[UIBarButtonItem alloc] initWithTitle:@"Tillbaka" style:UIBarButtonItemStyleBordered target:nil action:nil];
+//    // [btnDone setTintColor:[UIColor Color]];
+//    UIImage *stretchable = [UIImage imageNamed:@"tillbakabutton.png"] ;
+//    [btnDone setBackButtonBackgroundImage:stretchable forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+//    [self.navigationController.navigationItem setBackBarButtonItem:btnDone];
     
     //[self.view addSubview:scrollView];
     //scrollView.hidden=YES;
     // Do any additional setup after loading the view from its nib.
+    
+    
+    
+    // code changes by malkit to make navigation button appears like other view controllers
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        
+        UIImage *image = [UIImage imageNamed:@"tillbaka1.png"];
+        UIButton *okBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [okBtn setFrame:CGRectMake(0, 0, image.size.width, image.size.height)];
+        [okBtn setImage:image forState:UIControlStateNormal];
+        [okBtn addTarget:self action:@selector(backButon) forControlEvents:UIControlEventTouchUpInside];
+        
+        self.navigationItem.leftBarButtonItem =  [[UIBarButtonItem alloc] initWithCustomView:okBtn];
+        
+    }
+    else {
+        
+        UIImage *image = [UIImage imageNamed:@"tillbaka1.png"];
+        UIButton *okBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [okBtn setFrame:CGRectMake(0, 0, image.size.width, image.size.height)];
+        [okBtn setBackgroundImage:image forState:UIControlStateNormal];
+        [okBtn addTarget:self action:@selector(backButon) forControlEvents:UIControlEventTouchUpInside];
+        
+        self.navigationItem.leftBarButtonItem =  [[UIBarButtonItem alloc] initWithCustomView:okBtn];
+    }
+
 }
+-(void)backButon {
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 -(IBAction)pageB:(id)sender
 {
     Dinaomraden *dr;
