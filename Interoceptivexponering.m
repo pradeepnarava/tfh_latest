a9e1cab3b5f6497876b9674ef837821bc1799a29
Charanjit's Macbook Air <charanjitspc@charanjitss-MacBook-Air.local>
2013-10-07 07:15:55 +0530

    Changes done to utmana tankar and interocetiv

--- Interoceptivexponering.m
+++ Interoceptivexponering.m
@@ -66,6 +66,7 @@ int tagValueForBtn;
 
 -(void)dealloc{
     [hoursDisplay release];
+    [_RaderaButton release];
     [super dealloc];
     [timerQuestionLabel release];
     [allItems release];
@@ -853,6 +854,13 @@ int tagValueForBtn;
 }
 
 
+- (IBAction)Radera:(id)sender {
+    UIAlertView * alert=[[UIAlertView alloc] initWithTitle:nil message:@"Är du säker på att du vill radera formuläret?" delegate:self cancelButtonTitle:@"Radera" otherButtonTitles:@"Avbryt", nil];
+    alert.tag=kAlertViewTwo;
+    [alert show];
+    [alert release];
+}
+
 -(IBAction)SparaButton:(id)sender {
     
     
@@ -947,7 +955,47 @@ int tagValueForBtn;
             slider.value=0;
         }
     }
-    
+    else if(alertView.tag == kAlertViewTwo) {
+        if (buttonIndex == 0) {
+            
+            
+//            if (sqlite3_open([databasePath UTF8String], &exerciseDB) == SQLITE_OK) {
+            
+               // NSString *sql = [NSString stringWithFormat: @"DELETE FROM EXERCISE5 WHERE date='%@'", [listexercise3 objectAtIndex:y] ];
+                
+//                const char *del_stmt = [sql UTF8String];
+//                
+//                sqlite3_prepare_v2(exerciseDB, del_stmt, -1, & statement, NULL);
+//                if (sqlite3_step(statement) == SQLITE_ROW) {
+//                    
+//                    NSLog(@"sss");
+//                }
+//                
+//                sqlite3_finalize(statement);
+//                sqlite3_close(exerciseDB);
+                
+                
+//            }
+//            c1.text=@"";
+//            c2.text=@"";
+//            c3.text=@"0%";
+//            c4.text=@"";
+//            c5.text=@"";
+//            c6.text=@"";
+//            raderaButton.enabled=NO;
+//            [list_exercise3 removeAllObjects];
+//            y=0;
+//            [list_exercise3 addObject:@"Null"];
+//            [self getlistofDates3];
+            egen.text = @"";
+            ovning.text = @"";
+            
+            UIAlertView * alert1 = [[UIAlertView alloc] initWithTitle:nil message:@"Raderat" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Ok",nil];
+            [alert1 show];
+            [alert1 release];
+        }
+
+    }
 }
 
 
@@ -1037,6 +1085,7 @@ int tagValueForBtn;
 
 - (void)viewDidUnload{
     [self setHoursDisplay:nil];
+    [self setRaderaButton:nil];
     [super viewDidUnload];
 }
 
@@ -1046,7 +1095,7 @@ int tagValueForBtn;
 
 - (IBAction)skickaButtonClicked:(id)sender
 {
-    UIActionSheet *cameraActionSheet = [[UIActionSheet alloc] initWithTitle:@"Skicka" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Download", @"Email", nil];
+    UIActionSheet *cameraActionSheet = [[UIActionSheet alloc] initWithTitle:@"Skicka" delegate:self cancelButtonTitle:@"Avbryt" destructiveButtonTitle:nil otherButtonTitles:@"Ladda ner", @"E-mail", nil];
     cameraActionSheet.tag = 1;
     [cameraActionSheet showInView:self.view];
 }
